ba2fb42a1a4dddb61be5eca1df593a0ed626f448
franco-fernettepearl <fernettepearl.franco55@gmail.com>
2025-07-22 20:20:07 +0800

    make show password functional

--- web/src/static/js/app.js
+++ web/src/static/js/app.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     console.log("App loaded successfully");
 });
 
+function togglePassword() {
+  const passwordInput = document.getElementById('password');
+  const toggleBtn = document.querySelector('.toggle-password-btn i');
+  const isPassword = passwordInput.type === 'password';
+
+  passwordInput.type = isPassword ? 'text' : 'password';
+  toggleBtn.className = isPassword ? 'bi bi-eye-slash-fill' : 'bi bi-eye-fill';
+}
 
