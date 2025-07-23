e28ac7c6634461bccdc35a182dd9b134b9d70e39
franco-fernettepearl <fernettepearl.franco55@gmail.com>
2025-07-23 21:57:29 +0800

    create js injector

--- web/src/static/js/app.js
+++ web/src/static/js/app.js
@@ -11,3 +11,20 @@ function togglePassword() {
   toggleBtn.className = isPassword ? 'bi bi-eye-slash-fill' : 'bi bi-eye-fill';
 }
 
+function loadPostTemplate(data) {
+  $.get("post-template.html", function(template) {
+    const tagHTML = data.tags.map(tag => `<span class="tag">${tag}</span>`).join("");
+    template = template
+      .replace("{{username}}", data.username)
+      .replace("{{postTitle}}", data.title)
+      .replace("{{postContent}}", data.content)
+      .replace("{{timeAgo}}", data.timeAgo)
+      .replace("{{tags}}", tagHTML);
+
+    $(".feed-container").append(template);
+  });
+}
+
+$(document).ready(function() {
+  postData.forEach(post => loadPostTemplate(post));
+});
