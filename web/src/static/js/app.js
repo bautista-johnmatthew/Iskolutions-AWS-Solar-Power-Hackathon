599c7a5e1a3d7460bbc3358445384ec575b43c50
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2025-08-12 15:16:14 +0800

    Implement searching function

--- web/src/static/js/app.js
+++ web/src/static/js/app.js
@@ -1,12 +1,19 @@
 import { sessionManager } from "./managers/session-manager.js";
 import { feedManager } from './managers/feed-manager.js';
 import { filterPostsByTag } from "./utils/filter.js";
+import { fuzzySearchPosts } from "./post-api/postUtils.js"
 
 $(document).ready(function () {
     // Comment out this section to prevent feed initialization
     if (feedManager) {
         feedManager.initialize();
     }
+
+    $('.search-btn').on('click', function () {
+        const query = $('#search').val().toLowerCase();
+        const filteredPosts = fuzzySearchPosts(feedManager.posts, query);
+        feedManager.reloadPosts(filteredPosts, true);
+    });
 
     // Event listener for filtering buttons
     $('.filter-btn').on('click', function () {
